9d5bb2bfef56d812f8bcb7d5e8424df6ea4a11d1
balota-natan-utcn <balotann61@gmail.com>
2025-04-23 21:34:51 +0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # image-converter
 website for converting images
+
+
+commands to forcefully rebuild:
+{
+  git commit --allow-empty -m "Force rebuild"
+  git push origin main
+}
--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 document.getElementById('convertForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-  
-    const fileInput = document.getElementById('imageInput');
-    const format = document.getElementById('formatSelect').value;
-    const formData = new FormData();
-    formData.append('image', fileInput.files[0]);
-  
-    const res = await fetch(`https://image-converter-backend.onrender.com/convert?format=${format}`, {
-      method: 'POST',
-      body: formData
-    });
-  
-    const blob = await res.blob();
-    const url = window.URL.createObjectURL(blob);
-  
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `converted.${format}`;
-    a.click();
+  e.preventDefault();
+
+  const fileInput = document.getElementById('imageInput');
+  const format = document.getElementById('formatSelect').value;
+  const formData = new FormData();
+  formData.append('image', fileInput.files[0]);
+
+  const res = await fetch(`https://image-converter-backend-i6ei.onrender.com/convert?format=${format}`, {
+    method: 'POST',
+    body: formData
   });
-  
+
+  const blob = await res.blob();
+  const url = window.URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `converted.${format}`;
+  a.click();
+});
