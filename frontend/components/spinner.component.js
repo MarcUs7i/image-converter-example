e7818d903c06e3a34e4e6ed3056bc40f277f9654
balota-natan-utcn <balotann61@gmail.com>
2025-04-24 00:05:01 +0300

    added spinner component

---
+++ frontend/components/spinner.component.js
@@ -0,0 +1,17 @@
+export async function loadSpinner(targetSelector)
+{
+    const response = await fetch('spinner.component.html');
+    const html = await response.text();
+    document.querySelector(targetSelector).insertAdjacentHTML('beforeend', html);
+}
+  
+export function showSpinner()
+{
+    document.getElementById('loading-spinner')?.classList.remove('d-none');
+}
+  
+export function hideSpinner()
+{
+    document.getElementById('loading-spinner')?.classList.add('d-none');
+}
+  
